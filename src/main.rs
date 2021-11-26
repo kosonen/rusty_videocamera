5b78e7fd26a43b6f2e16883b6acd2262549c0d81
Antti Nurmi <antti.nurmi@tuni.fi>
2021-11-26 16:46:27 +0200

    Add read pixel data

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 #![no_main]
 
 
+use core::convert::{Infallible};
+use core::pin;
+
 use longan_nano::hal::gpio::{Floating, Input, gpioa, gpioc};
 use riscv_rt::entry;
 use panic_halt as _;
@@ -12,6 +15,20 @@ use longan_nano::hal::{pac, prelude::*, gpio::gpioc::*, gpio::gpioa::*};
 use longan_nano::{lcd_pins,lcd};
 use embedded_hal::digital::v2::{InputPin, OutputPin, ToggleableOutputPin};
 use longan_nano::hal::delay::McycleDelay;
+
+pub struct DataPins
+{
+    pin0 : PA2<Input<Floating>>,
+    pin1 : PA1<Input<Floating>>,
+    pin2 : PA0<Input<Floating>>,
+    pin3 : PC13<Input<Floating>>,
+    pin4 : PC14<Input<Floating>>,
+    pin5 : PC15<Input<Floating>>,
+    pin6 : PA3<Input<Floating>>,
+    pin7 : PA4<Input<Floating>>
+
+}
+
 
 #[entry]
 fn main() -> ! {
@@ -40,23 +57,17 @@ fn main() -> ! {
         pin3 : gpioc.pc13,
         pin4 : gpioc.pc14,
         pin5 : gpioc.pc15,
+        pin6 : gpioa.pa3,
+        pin7 : gpioa.pa4,
     };
     let mut clkPin = gpiob.pb9.into_push_pull_output();
-    let mut clk = false;
     let mut delay = McycleDelay::new(&rcu.clocks);
-
+    let mut rgb_bytes = [[0u8 ; 160]; 70];
     loop 
     {
-        if(!clk)
-        {
-            clk = true;
-            clkPin.set_high();
-        }
-        else {
-            clk = false;
-            clkPin.set_low();
-        }
-
+        clkPin.set_high();
+        clkPin.set_low();
+        
         let datapin7Status = match dataPins.pin5.is_high()
         {
             Ok(f) =>  f,
@@ -67,8 +78,7 @@ fn main() -> ! {
             Ok(f) =>  f,
             Err(e) => false,
         };
-        let value = evaluate_data(datapin7Status, datapin6Status);
-
+        let mut val = read_pixel(&dataPins, &rgb_bytes, (1,1));
         if(!datapin7Status)
         {
             Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
@@ -83,63 +93,39 @@ fn main() -> ! {
             .draw(&mut lcd)
             .unwrap();
         }
-  /*  
-        match value {
-            0 => Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
-            .draw(&mut lcd)
-            .unwrap(),
-            1 => Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::MAGENTA))
-            .draw(&mut lcd)
-            .unwrap(),
-            2 => Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::BLUE))
-            .draw(&mut lcd)
-            .unwrap(),
-            3 => Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
-            .draw(&mut lcd)
-            .unwrap(),
-            _ => Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::RED))
-            .draw(&mut lcd)
-            .unwrap(),
-        }; 
-        */
-
-        delay.delay_ms(500);
+        delay.delay_ms(20000);
 
     }
 }
 
-fn evaluate_data(pin1: bool, pin2: bool) -> u8
+fn read_pixel(pins : &DataPins, array: &[[u8 ; 160]; 70], (row,column): (u8, u8) ) -> u8
 {
-    if(!pin1 && !pin2)
-    {
-        return 0;
-    }
-    else if(pin1 && !pin2)
-    {
-        return 1;
-    }
-    else if(!pin1 && pin2)
-    {
-        return 2;
-    }
-    else {
-        return 3;
-    }
-}   
-
-pub struct DataPins
-{
-    pin0 : PA2<Input<Floating>>,
-    pin1 : PA1<Input<Floating>>,
-    pin2 : PA0<Input<Floating>>,
-    pin3 : PC13<Input<Floating>>,
-    pin4 : PC14<Input<Floating>>,
-    pin5 : PC15<Input<Floating>>,
-
+    let mut rgb_value: u8 = 0; 
+    rgb_value &= read_pin(pins.pin7.is_high()) as u8;
+    rgb_value = rgb_value << 1;
+    rgb_value &= read_pin(pins.pin6.is_high()) as u8;
+    rgb_value = rgb_value << 1;
+    rgb_value &= read_pin(pins.pin5.is_high()) as u8;
+    rgb_value = rgb_value << 1;
+    rgb_value &= read_pin(pins.pin4.is_high()) as u8;
+    rgb_value = rgb_value << 1;
+    rgb_value &= read_pin(pins.pin3.is_high()) as u8;
+    rgb_value = rgb_value << 1;
+    rgb_value &= read_pin(pins.pin2.is_high()) as u8;
+    rgb_value = rgb_value << 1;
+    rgb_value &= read_pin(pins.pin1.is_high()) as u8;
+    rgb_value = rgb_value << 1;
+    rgb_value &= read_pin(pins.pin0.is_high()) as u8;
+    rgb_value
 }
+
+fn read_pin(is_pin_high : Result<bool, Infallible>) -> bool
+{
+    let mut retVal = match is_pin_high {
+        Ok(f) =>  f,
+        Err(e) => false,
+    };
+    retVal
+}
+
 
