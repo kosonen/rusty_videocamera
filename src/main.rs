b263a2e3d7683c3064c60e6599426f01a8fd5a13
Henri Kosonen <henri.kosonen@tuni.fi>
2021-12-09 14:29:31 +0200

    Add clock signal for camera module

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,39 @@
 #![no_std]
 #![no_main]
 
-
-use core::convert::{Infallible};
-use core::pin;
-
-use longan_nano::hal::gpio::{Floating, Input, gpioa, gpioc};
+use longan_nano::hal::eclic::{EclicExt, Level, LevelPriorityBits, Priority, TriggerType};
+use longan_nano::hal::gpio::gpiob::PB9;
+use longan_nano::hal::gpio::{Floating, Input, OpenDrain, Output, PullDown, PushPull, State};
+use longan_nano::hal::timer::{Event, Timer};
+use longan_nano::lcd::Lcd;
 use riscv_rt::entry;
 use panic_halt as _;
 use embedded_graphics::prelude::*;
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::primitives::{Rectangle, PrimitiveStyle};
+use embedded_graphics::pixelcolor::{ Rgb565};
 use longan_nano::hal::{pac, prelude::*, gpio::gpioc::*, gpio::gpioa::*};
 use longan_nano::{lcd_pins,lcd};
-use embedded_hal::digital::v2::{InputPin, OutputPin, ToggleableOutputPin};
+use embedded_hal::digital::v2::{InputPin, OutputPin, StatefulOutputPin};
 use longan_nano::hal::delay::McycleDelay;
 
 pub struct DataPins
 {
-    pin0 : PA2<Input<Floating>>,
-    pin1 : PA1<Input<Floating>>,
-    pin2 : PA0<Input<Floating>>,
-    pin3 : PC13<Input<Floating>>,
-    pin4 : PC14<Input<Floating>>,
-    pin5 : PC15<Input<Floating>>,
-    pin6 : PA3<Input<Floating>>,
-    pin7 : PA4<Input<Floating>>
+    pin0 : PA2<Input<PullDown>>,
+    pin1 : PA1<Input<PullDown>>,
+    pin2 : PA0<Input<PullDown>>,
+    pin3 : PC13<Input<PullDown>>,
+    pin4 : PC14<Input<PullDown>>,
+    pin5 : PC15<Input<PullDown>>,
+    pin6 : PA3<Input<PullDown>>,
+    pin7 : PA4<Input<PullDown>>
 
 }
 
+const NUMBER_OF_ROWS: u8 = 160;
+const NUMBER_OF_COLUMNS : u8 = 80;
+const NUMBER_OF_PIXELS : u16 = 12800;
+static mut CLK_PIN : Option<PA9<Output<PushPull>>> = None;
+static mut CAMERA_CLOCK_STATE : bool = false;
+static mut TIMER : Option<Timer<longan_nano::hal::pac::TIMER1>> = None;
 
 #[entry]
 fn main() -> ! {
@@ -40,92 +45,97 @@ fn main() -> ! {
     .sysclk(108.mhz())
     .freeze();
 
+    
     let mut afio = dp.AFIO.constrain(&mut rcu);
 
     let gpioa = dp.GPIOA.split(&mut rcu);
     let gpiob = dp.GPIOB.split(&mut rcu);
- 
+    
+    let gpioc = dp.GPIOC.split(&mut rcu);
+
+
     let lcd_pins = lcd_pins!(gpioa, gpiob);
     let mut lcd = lcd::configure(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
     let (width, height) = (lcd.size().width as i32, lcd.size().height as i32);
-    let gpioc = dp.GPIOC.split(&mut rcu);
-    //let mut dataPins= create_datapins(gpioc, gpioa);
-    let mut dataPins = DataPins{
-        pin0 : gpioa.pa2,
-        pin1 : gpioa.pa1,
-        pin2 : gpioa.pa0,
-        pin3 : gpioc.pc13,
-        pin4 : gpioc.pc14,
-        pin5 : gpioc.pc15,
-        pin6 : gpioa.pa3,
-        pin7 : gpioa.pa4,
+    lcd.clear(Rgb565::BLACK).unwrap();
+     let data_pins = DataPins{
+        pin0 : gpioa.pa2.into_pull_down_input(),
+        pin1 : gpioa.pa1.into_pull_down_input(),
+        pin2 : gpioa.pa0.into_pull_down_input(),
+        pin3 : gpioc.pc13.into_pull_down_input(),
+        pin4 : gpioc.pc14.into_pull_down_input(),
+        pin5 : gpioc.pc15.into_pull_down_input(),
+        pin6 : gpioa.pa3.into_pull_down_input(),
+        pin7 : gpioa.pa4.into_pull_down_input(),
     };
-    let mut clkPin = gpiob.pb9.into_push_pull_output();
+    let mut clk_pin = gpioa.pa9.into_push_pull_output_with_state(State::Low);
     let mut delay = McycleDelay::new(&rcu.clocks);
-    let mut rgb_bytes = [[0u8 ; 160]; 70];
-    loop 
-    {
-        clkPin.set_high();
-        clkPin.set_low();
-        
-        let datapin7Status = match dataPins.pin5.is_high()
-        {
-            Ok(f) =>  f,
-            Err(e) => false,
-        };
-        let datapin6Status = match dataPins.pin4.is_high()
-        {
-            Ok(f) =>  f,
-            Err(e) => false,
-        };
-        let mut val = read_pixel(&dataPins, &rgb_bytes, (1,1));
-        if(!datapin7Status)
-        {
-            Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
-            .draw(&mut lcd)
-            .unwrap();
-        }
-        else 
-        {
-            Rectangle::new(Point::new(0, 0), Size::new(width as u32, height as u32))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
-            .draw(&mut lcd)
-            .unwrap();
-        }
-        delay.delay_ms(20000);
 
+    let href = gpioa.pa12;
+    let read_clk = gpiob.pb9;
+
+    let mut rgb_bytes = [[0u8 ; NUMBER_OF_ROWS as usize]; NUMBER_OF_COLUMNS as usize];
+    let mut counter_row: u16 = 0;
+    let mut counter_column: u16 = 0;
+    let mut test_bytes = [0u8; 255];
+    let mut test_counter: usize = 0;
+ 
+    
+    longan_nano::hal::pac::ECLIC::reset();
+    longan_nano::hal::pac::ECLIC::set_level_priority_bits(LevelPriorityBits::L3P1);
+    longan_nano::hal::pac::ECLIC::set_threshold_level(Level::L0);
+    longan_nano::hal::pac::ECLIC::setup(pac::Interrupt::TIMER1, TriggerType::Level, Level::L1, Priority::P1);
+     
+    let mut timer = Timer::timer1(dp.TIMER1, 24.mhz(), &mut rcu);  
+    timer.listen(Event::Update);
+
+     unsafe{
+        TIMER = Some(timer);
+        CLK_PIN = Some(clk_pin);
+        longan_nano::hal::pac::ECLIC::unmask(pac::Interrupt::TIMER1);
+        riscv::interrupt::enable();
+    }
+    
+    loop {
+        unsafe
+        {   
+            riscv::asm::wfi();
+            // Clock rises   
+            if CAMERA_CLOCK_STATE
+            {
+            }
+            // Clock lowers
+            else 
+            {
+            }
+        }
+        
     }
 }
 
-fn read_pixel(pins : &DataPins, array: &[[u8 ; 160]; 70], (row,column): (u8, u8) ) -> u8
+#[allow(non_snake_case)]
+#[allow(non_upper_case_globals)]
+#[allow(unused_assignments)]
+#[no_mangle]
+fn TIMER1 ()
 {
-    let mut rgb_value: u8 = 0; 
-    rgb_value &= read_pin(pins.pin7.is_high()) as u8;
-    rgb_value = rgb_value << 1;
-    rgb_value &= read_pin(pins.pin6.is_high()) as u8;
-    rgb_value = rgb_value << 1;
-    rgb_value &= read_pin(pins.pin5.is_high()) as u8;
-    rgb_value = rgb_value << 1;
-    rgb_value &= read_pin(pins.pin4.is_high()) as u8;
-    rgb_value = rgb_value << 1;
-    rgb_value &= read_pin(pins.pin3.is_high()) as u8;
-    rgb_value = rgb_value << 1;
-    rgb_value &= read_pin(pins.pin2.is_high()) as u8;
-    rgb_value = rgb_value << 1;
-    rgb_value &= read_pin(pins.pin1.is_high()) as u8;
-    rgb_value = rgb_value << 1;
-    rgb_value &= read_pin(pins.pin0.is_high()) as u8;
-    rgb_value
+    unsafe{
+        TIMER.as_mut().unwrap().clear_update_interrupt_flag();
+        if !CAMERA_CLOCK_STATE
+        {
+            CAMERA_CLOCK_STATE = true;
+            CLK_PIN.as_mut().unwrap().set_high().unwrap();
+        }
+        else
+        {
+            CAMERA_CLOCK_STATE = false;
+            CLK_PIN.as_mut().unwrap().set_low().unwrap();
+        }
+         
+
+        
+        
+    }
+    
+
 }
-
-fn read_pin(is_pin_high : Result<bool, Infallible>) -> bool
-{
-    let mut retVal = match is_pin_high {
-        Ok(f) =>  f,
-        Err(e) => false,
-    };
-    retVal
-}
-
-
